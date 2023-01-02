e2ba9fde17712e32b95eadbc6f2a5d5f29b23d77
Noufal Aboobacker <noufionline@gmail.com>
2023-01-02 08:48:08 +0000

    added metric server

--- infra/k8s/api/apply.sh
+++ infra/k8s/api/apply.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 kubectl apply -f service.yaml
+kubectl apply -f hpa-by-cpu.yaml
+kubectl apply -f hpa-by-memory.yaml
 kubectl apply -f deployment.yaml
--- infra/k8s/api/clear.sh
+++ infra/k8s/api/clear.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+kubectl delete -f metric-server.yaml
 kubectl delete -f deployment.yaml
 kubectl delete -f service.yaml
