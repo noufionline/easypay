9bb6dd2ae8376567cb2cb7f4691d7871ffb836cc
Noufal Aboobacker <noufionline@gmail.com>
2023-01-07 19:48:02 +0400

    Update backup.sh

--- infra/k8s/etcd-snapshot/backup.sh
+++ infra/k8s/etcd-snapshot/backup.sh
@@ -1,7 +1,7 @@
 #!//bin/bash
+export ETCD_POD=$(kubectl get pods -n kube-system | grep etcd | awk '{print $1}' | head -n 1)
 
-
-export RELEASE=$(kubectl exec -it etcd-k8s-master1 -n kube-system -- /bin/sh -c 'ETCDCTL_API=3 /usr/local/bin/etcd --version' | head -n 1 | awk '{ print $3 }')
+export RELEASE=$(kubectl exec -it ${ETCD_POD} -n kube-system -- /bin/sh -c 'ETCDCTL_API=3 /usr/local/bin/etcd --version' | head -n 1 | awk '{ print $3 }')
 
 wget https://github.com/etcd-io/etcd/releases/download/v${RELEASE}/etcd-v${RELEASE}-linux-amd64.tar.gz
 
