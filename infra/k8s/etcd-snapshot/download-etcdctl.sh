29056d9a9686ff98e39dde3cd9b7818045b36ba0
Noufal Aboobacker <noufionline@gmail.com>
2023-01-08 12:14:39 +0400

    Update download-etcdctl.sh

--- infra/k8s/etcd-snapshot/download-etcdctl.sh
+++ infra/k8s/etcd-snapshot/download-etcdctl.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
-set -x
+#Get the etcd pod name
+etcd_pod=$(kubectl get pods -n kube-system | grep etcd | awk '{ print $1 }')
 
-# Create temp folder
-WORK_DIR=/tmp/etcd
-[ -d ${WORK_DIR} ] || mkdir -p ${WORK_DIR}
-cd ${WORK_DIR}
+# Store etcd version to a variable
 
-# Get the latest from the releases folder
-curl -s https://api.github.com/repos/etcd-io/etcd/releases/latest |\
-  grep browser_download_url |\
-  grep linux-amd64 |\
-  cut -d '"' -f 4 | wget -qi -
+release=$(kubectl exec -it etcd-k8s-master1 -n kube-system -- /bin/sh -c 'ETCDCTL_API=3 /usr/local/bin/etcd --version' | awk '{ print $3 }' | head -1 | sed 's/\r$//')
 
-# Extract the archive
-tar xvf *.tar.gz
 
-# Install in /usr/local/bin
-cd etcd-*/
-sudo mv etcd* /usr/local/bin/
 
-# Cleanup
-cd ..
+echo "The release used inside the ${etcd_pod} is ${release}"
+
+echo "[TASK 1] Download the version ${release}"
+echo
+
+wget  https://github.com/etcd-io/etcd/releases/download/v${release}/etcd-v${release}-linux-amd64.tar.gz
+
+echo 
+
+echo "[TASK 2] Extract the zip file"
+echo
+
+tar -zxvf etcd-v${release}-linux-amd64.tar.gz
+
+echo 
+
+echo "[TASK 3] Install in /usr/local/bin"
+echo
+sudo mv etcd-v${release}-linux-amd64/etcdctl /usr/local/bin
+
+echo
+
+echo "[TASK 4] Check the installed version"
+echo
+etcdctl version
+echo
+
+
+echo "[TASK 5] Cleanup"
+echo
+
 rm -rf *.tar.gz etcd-*/
