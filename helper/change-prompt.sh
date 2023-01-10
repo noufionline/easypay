9f1fab61d4b3420080fc6807168db3f6fa4291c8
Noufal Aboobacker <noufionline@gmail.com>
2023-01-10 09:59:51 +0400

    startship prompt details added in change-prompt.sh

--- helper/change-prompt.sh
+++ helper/change-prompt.sh
@@ -2,4 +2,14 @@
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
+# Recommended and currently using
+
+# https://starship.rs/
+
+
+curl -sS https://starship.rs/install.sh | sh
+
+#Add the following to the end of ~/.bashrc:
+
+eval "$(starship init bash)"
 
